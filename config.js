0cf0035c068b22a2c50a5b9b73c30de424216303
talisis2025-lab <talisis2025@gmail.com>
2026-08-19 17:46:39 -0600

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Pega aquí la URL /exec de la NUEVA implementación de Apps Script.
-// No reutilices la implementación de la página individual anterior.
-window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwPF9ZeyOgz4ps_iBnChCuq-umIoykmeh4DlVRcbwrrlFpcncq-GhnhHqwNcDS3188e/exec"
-};
